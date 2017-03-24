f03652cb08869a994f7a0df89025438737f7f51d
Abdelkrime Aries <kariminfo0@gmail.com>
2017-03-24 15:38:09 +0100

    documentation

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-Edict2Ext
-=========
+# KInfoMiner
 
-Parsing and Extracting information from Jim Breen's Edict2 dictionary
+[![Project](https://img.shields.io/badge/Project-KInfoMiner-4B0082.svg?style=plastic)](http://ararud.sourceforge.net)
+[![Version](https://img.shields.io/badge/Version-1.0.0-4B0082.svg?style=plastic)](https://github.com/kariminf/ArArud/releases)
+[![License](https://img.shields.io/badge/License-Apache_2-4B0082.svg?style=plastic)](http://www.apache.org/licenses/LICENSE-2.0)
+[![Travis](https://img.shields.io/travis/kariminf/KInfoMiner.svg?style=plastic)](https://travis-ci.org/kariminf/KInfoMiner)
+
+**Kariminf Infomation Miner**
+
+Was, first, designed as a parser to extract information from [Jim Breen's Edict2 dictionary](http://www.edrdg.org/jmdict/edict_doc.html).  
+But, why not create a global project and extend this one with a wider vision:
+* Create parsers for all sort of dictionaries and files
+* Save the extracted information in a database (current sqlite)
+
+
+## License
+
+Copyright (C) 2013,2017 Abdelkrime Aries
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
--- test/kariminf/kinfominer/edict2/VerbExtract.java
+++ test/kariminf/kinfominer/edict2/VerbExtract.java
@@ -39,16 +39,16 @@ import kariminf.kinfominer.edict2.lex.MEANS;
 
 
 public class VerbExtract {
-	
+
 	private static Connection con;
-	
+
 	private final String workplace = "/home/kariminf/Data/jap/";
-	
+
 	private final String verbstype = "v1|v2a-s|v4h|v4r|v5aru|v5b|v5g|v5k-s|v5k|v5m" +
 			"|v5n|v5r-i|v5r|v5s|v5t|v5u-s|v5uru|v5u|v5z|v5|vz|vk|vn|vs-c|vs-i|vs-s|vs";
-	
+
 	private HashMap<String,Integer> Ntype = new HashMap<String,Integer>();
-	
+
 	private static final String[][] Vtype = {
 		{"v1",			"Ichidan verb"},
 		{"v2a-s",		"Nidan verb with 'u' ending (archaic)"},
@@ -77,18 +77,18 @@ public class VerbExtract {
 		{"vs-c",		"su verb - precursor to the modern suru"},
 		{"vs-i",		"suru verb - irregular"},
 		{"vs-s",		"suru verb - special class"}
-			
+
 	};
-	
+
 	private Pattern parse = Pattern.compile(".*\\(.*("+verbstype+").*\\).*");	
-	
+
 	public void insert_type() throws Exception {
-		
+
 		//Statement stat = con.createStatement();
 		int num = 1;
 		PreparedStatement prep = con
 				.prepareStatement("insert into ttype values(?,?,?);");
-		
+
 		for (String[] vtype: Vtype)
 		{
 			prep.setString(2, vtype[0]);
@@ -97,165 +97,181 @@ public class VerbExtract {
 			Ntype.put(vtype[0], num);
 			num++;
 		}
-		
+
 	}
-	
-	
+
+
 	public void insert_verbs() throws Exception {
-		
+
 		Statement stat = con.createStatement();
 		ResultSet res;
-		
-		//String NL = System.getProperty("line.separator");
-	    Scanner scanner = new Scanner(new FileInputStream(workplace + "edict2u"), "UTF-8");
-	    int numkana = 1;
-	    int numkanji = 1;
-	    
-	    //ArrayList<String> ckana;
-	    //ArrayList<String> ckanji;
-	    	    
-	    try {
-		      while (scanner.hasNextLine()){
-		      	String oldline = scanner.nextLine();
-		      	Matcher m = parse.matcher(oldline);
-		      	//oldline.matches(".*\\((.*"+verbstype+".*)+\\).*")
-		      	if (m.find())
-		      	{
-		      		LINE parsedline = Parser.ParseLINE(oldline);
-		      		
-		      		String[] kanjis = parsedline.getKanjiList();
-		    		String[] kanas = parsedline.getKanaList();
-		    		String add = "";
-		    		
-		    		String thetype = m.group(1);
-		    		int ntype = 0;
-		    		
-		    		ntype = (Ntype.containsKey(thetype.trim()))?Ntype.get(thetype.trim()):0;
-		    		System.out.println("------> "+ thetype + " = " + ntype);	
-		    		
-		    		if (ntype==24)
-		    			add = "する";
-		    		 		
-		    		PreparedStatement prep = con
-		    				.prepareStatement("insert into tkanji values(?,?,?);");
-		    		
-		    		for(String kanji:kanjis)
-		    		{
-		    			res = stat.executeQuery("select idkanji from tkanji where kanji ='" 
-		    					+ kanji + add + "';");
-		    			
-		    			if (res.next())
-		    			{
-		    				//ckanji.add(res.getString("idkanji"));
-		    				System.out.println("kanji: '"+ kanji + "' is already in.");
-		    			}
-		    			else
-		    			{
-		    				prep.setString(1, "" + numkanji);
-		    				//ckanji.add("" + numkanji);
-		    				numkanji++;
-		    				prep.setString(2, kanji+add);
-		    				prep.setString(3, ""+ntype);
-		    				prep.execute();
-		    				System.out.println("kanji: '"+ kanji + "' is in.");
-		    			}
-		    			
-		    		}
-		    		
-		    		prep = con
-		    				.prepareStatement("insert into tkana values(?,?,?);");
-		    		
-		    		for(String kana:kanas)
-		    		{
-		    			res = stat.executeQuery("select idkana from tkana where kana ='" 
-		    					+ kana + add + "';");
-		    			
-		    			if (res.next())
-		    			{
-		    				//ckanji.add(res.getString("idkana"));
-		    				System.out.println("kana: '"+ kana + "' is already in.");
-		    			}
-		    			else
-		    			{
-		    				prep.setString(1, ("" + numkana));
-		    				//ckana.add("" + numkana);
-		    				numkana++;
-		    				prep.setString(2, kana+add);
-		    				prep.setString(3, ""+ntype);
-		    				prep.execute();
-		    				System.out.println("kana: '"+ kana + "' is in.");
-		    			}
-		    			
-		    		}
-		    		
-		    		prep = con
-		    				.prepareStatement("insert into tread values(?,?);");
-		    		
-		    		for(String kanji:kanjis){
-		    			res = stat.executeQuery("select idkanji from tkanji where kanji ='" 
-		    					+ kanji + add + "';");
-		    			int kanjiid = 0;
-		    			if (res.next()) kanjiid = res.getInt(1);
-		    			
-		    			for(String kana:kanas){
-		    				res = stat.executeQuery("select idkana from tkana where kana ='" 
-			    					+ kana + add + "';");
-			    			int kanaid = 0;
-			    			if (res.next()) kanaid = res.getInt(1);
-			    			
-			    			prep.setInt(1, kanjiid);
-		    				prep.setInt(2, kanaid);
-		    				prep.execute();
-		    				System.out.println("kanji:"+ kanjiid + " = kana:" + kanaid);
-			    		}
-		    		}
-		    		
-		      	}
 
-		      }
-		    }
-		    finally{
-		      scanner.close();
-		    }
-		
+		//String NL = System.getProperty("line.separator");
+		Scanner scanner = new Scanner(new FileInputStream(workplace + "edict2u"), "UTF-8");
+		int numkana = 1;
+		int numkanji = 1;
+
+		//ArrayList<String> ckana;
+		//ArrayList<String> ckanji;
+
+		try {
+			while (scanner.hasNextLine()){
+				String oldline = scanner.nextLine();
+				Matcher m = parse.matcher(oldline);
+				//oldline.matches(".*\\((.*"+verbstype+".*)+\\).*")
+				if (m.find())
+				{
+					LINE parsedline = Parser.ParseLINE(oldline);
+
+					String[] kanjis = parsedline.getKanjiList();
+					String[] kanas = parsedline.getKanaList();
+					
+					ArrayList<Integer> kanjisNbr = new ArrayList<>();
+					ArrayList<Integer> kanaNbr = new ArrayList<>();
+					
+					String add = "";
+
+					String thetype = m.group(1);
+					int ntype = 0;
+
+					ntype = (Ntype.containsKey(thetype.trim()))?Ntype.get(thetype.trim()):0;
+					System.out.println("------> "+ thetype + " = " + ntype);	
+
+					if (ntype==24)
+						add = "する";
+
+					PreparedStatement prep = con
+							.prepareStatement("insert into tkanji values(?,?,?);");
+
+					for(String kanji:kanjis)
+					{
+						res = stat.executeQuery("select idkanji from tkanji where kanji ='" 
+								+ kanji + add + "';");
+
+						if (res.next())
+						{
+							kanjisNbr.add(res.getInt("idkanji"));
+							//ckanji.add(res.getString("idkanji"));
+							System.out.println("kanji: '"+ kanji + "' is already in.");
+						}
+						else
+						{
+							prep.setString(1, "" + numkanji);
+							kanjisNbr.add(numkanji);
+							//ckanji.add("" + numkanji);
+							numkanji++;
+							prep.setString(2, kanji+add);
+							prep.setString(3, ""+ntype);
+							prep.execute();
+							System.out.println("kanji: '"+ kanji + "' is in.");
+						}
+
+					}
+
+					prep = con
+							.prepareStatement("insert into tkana values(?,?,?);");
+
+					for(String kana:kanas)
+					{
+						res = stat.executeQuery("select idkana from tkana where kana ='" 
+								+ kana + add + "';");
+
+						if (res.next())
+						{
+							//ckanji.add(res.getString("idkana"));
+							kanaNbr.add(res.getInt("idkana"));
+							System.out.println("kana: '"+ kana + "' is already in.");
+						}
+						else
+						{
+							prep.setString(1, ("" + numkana));
+							kanaNbr.add(numkana);
+							//ckana.add("" + numkana);
+							numkana++;
+							prep.setString(2, kana+add);
+							prep.setString(3, ""+ntype);
+							prep.execute();
+							System.out.println("kana: '"+ kana + "' is in.");
+						}
+
+					}
+
+					prep = con
+							.prepareStatement("insert into tread values(?,?);");
+
+					for(int kanjiid:kanjisNbr){
+						for(int kanaid:kanaNbr){
+							prep.setInt(1, kanjiid);
+							prep.setInt(2, kanaid);
+							prep.execute();
+							System.out.println("kanji:"+ kanjiid + " = kana:" + kanaid);
+						}
+					}
+					/*for(String kanji:kanjis){
+						res = stat.executeQuery("select idkanji from tkanji where kanji ='" 
+								+ kanji + add + "';");
+						int kanjiid = 0;
+						if (res.next()) kanjiid = res.getInt(1);
+
+						for(String kana:kanas){
+							res = stat.executeQuery("select idkana from tkana where kana ='" 
+									+ kana + add + "';");
+							int kanaid = 0;
+							if (res.next()) kanaid = res.getInt(1);
+
+							prep.setInt(1, kanjiid);
+							prep.setInt(2, kanaid);
+							prep.execute();
+							System.out.println("kanji:"+ kanjiid + " = kana:" + kanaid);
+						}
+					}*/
+
+				}
+
+			}
+		}
+		finally{
+			scanner.close();
+		}
+
 	}
-	
+
 	public void create() throws Exception {
 
-		  Class.forName("org.sqlite.JDBC");
-		  con = DriverManager.getConnection("jdbc:sqlite:" + workplace + "edict2verbs.db");
-		  Statement stat = con.createStatement();
-		  stat.executeUpdate("drop table if exists tkanji");
-		  stat.executeUpdate("drop table if exists tkana");
-		  stat.executeUpdate("drop table if exists tread");
-		  stat.executeUpdate("drop table if exists ttype");
+		Class.forName("org.sqlite.JDBC");
+		con = DriverManager.getConnection("jdbc:sqlite:" + workplace + "edict2verbs.db");
+		Statement stat = con.createStatement();
+		stat.executeUpdate("drop table if exists tkanji");
+		stat.executeUpdate("drop table if exists tkana");
+		stat.executeUpdate("drop table if exists tread");
+		stat.executeUpdate("drop table if exists ttype");
 
-		  //creating table kanji
-		  stat.executeUpdate("create table tkanji(idkanji integer,"
-		    + "kanji varchar(20)," + "idtype INT," + "primary key (idkanji));");
-		  stat.executeUpdate("create unique index idx_tkanji "
-				    + "on tkanji (kanji asc);");
-		 
+		//creating table kanji
+		stat.executeUpdate("create table tkanji(idkanji integer,"
+				+ "kanji varchar(20)," + "idtype INT," + "primary key (idkanji));");
+		stat.executeUpdate("create unique index idx_tkanji "
+				+ "on tkanji (kanji asc);");
+
 		//creating table kana
-		  stat.executeUpdate("create table tkana(idkana integer,"
-		    + "kana varchar(20)," + "idtype INT," + "primary key (idkana));");
-		  stat.executeUpdate("create unique index idx_tkana "
-				    + "on tkana (kana asc);");
-		  
-		  
-		//creating table reading
-		  stat.executeUpdate("create table tread(idkanji integer,"
-		    + "idkana integer);");
-	
-		//creating table type
-		  stat.executeUpdate("create table ttype(idtype integer,"
-		    + "acrotype varchar(5)," + "type varchar(40),"
-				  + "primary key (idtype));");
-		  stat.executeUpdate("create unique index idx_ttype "
-				    + "on ttype (acrotype asc);");
+		stat.executeUpdate("create table tkana(idkana integer,"
+				+ "kana varchar(20)," + "idtype INT," + "primary key (idkana));");
+		stat.executeUpdate("create unique index idx_tkana "
+				+ "on tkana (kana asc);");
 
-		 		 
-		  /* getting data
+
+		//creating table reading
+		stat.executeUpdate("create table tread(idkanji integer,"
+				+ "idkana integer);");
+
+		//creating table type
+		stat.executeUpdate("create table ttype(idtype integer,"
+				+ "acrotype varchar(5)," + "type varchar(40),"
+				+ "primary key (idtype));");
+		stat.executeUpdate("create unique index idx_ttype "
+				+ "on ttype (acrotype asc);");
+
+
+		/* getting data
 		  ResultSet res = stat.executeQuery("select * from user");
 		  while (res.next()) {
 		     System.out.println(res.getString("id") + " " + res.getString("age")
@@ -263,23 +279,23 @@ public class VerbExtract {
 		              + res.getString("sex") + " " + res.getString("weight")
 		              + " " + res.getString("height"));
 		  }*/
-		}
-	
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		VerbExtract p2sqlite = new VerbExtract();
-		
+
 		try {
-		      p2sqlite.create();
-		      p2sqlite.insert_type();
-		      p2sqlite.insert_verbs();
-		      con.close();
-		    } catch (Exception e) {
-		       e.printStackTrace();
-		    }
+			p2sqlite.create();
+			p2sqlite.insert_type();
+			p2sqlite.insert_verbs();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
